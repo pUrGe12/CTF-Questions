1baf119134c69411888966a61b4d1b7ae7ba9dcf
Achintya Jai <153343775+pUrGe12@users.noreply.github.com>
2024-08-14 22:16:51 +0530

    Update question.cpp

--- Rev/HuffingAndPuffing/question.cpp
+++ Rev/HuffingAndPuffing/question.cpp
@@ -23,63 +23,6 @@ Code for Huffman taken from lots of online resources
 3. https://www.geeksforgeeks.org/huffman-coding-using-priority-queue 
 */
 
-struct HuffmanNode {
-    char data;
-    unsigned frequency;
-    HuffmanNode* left;
-    HuffmanNode* right;
-
-    HuffmanNode(char data, unsigned frequency)
-        : data(data), frequency(frequency), left(nullptr), right(nullptr) {}
-
-    ~HuffmanNode() {
-        delete left;
-        delete right;
-    }
-};
-
-struct Compare {
-    bool operator()(HuffmanNode* left, HuffmanNode* right) {
-        return left->frequency > right->frequency;
-    }
-};
-
-HuffmanNode* buildHuffmanTree(const std::unordered_map<char, unsigned>& freqMap) {
-    std::priority_queue<HuffmanNode*, std::vector<HuffmanNode*>, Compare> pq;
-
-    for (const auto& pair : freqMap) {
-        pq.push(new HuffmanNode(pair.first, pair.second));
-    }
-
-    while (pq.size() != 1) {
-        HuffmanNode* left = pq.top(); pq.pop();
-        HuffmanNode* right = pq.top(); pq.pop();
-        HuffmanNode* parent = new HuffmanNode('\0', left->frequency + right->frequency);
-        parent->left = left;
-        parent->right = right;
-        pq.push(parent);
-    }
-
-    return pq.top();
-}
-
-void buildCodes(HuffmanNode* root, const std::string& code, std::unordered_map<char, std::string>& codes) {
-    if (!root)
-        return;
-    if (root->data != '\0')
-        codes[root->data] = code;
-    buildCodes(root->left, code + "0", codes);
-    buildCodes(root->right, code + "1", codes);
-}
-
-std::string encode(const std::string& data, const std::unordered_map<char, std::string>& codes) {
-    std::string encoded;
-    for (char c : data) {
-        encoded += codes.at(c);
-    }
-    return encoded;
-}
-
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //                                                                                        Helper functions
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
